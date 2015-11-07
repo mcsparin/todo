8fc4973200c4854d6ec73ad2e882cf5a68c35e00
mcsparin <jan.mcsparin@gmail.com>
2015-11-07 15:30:03 -0700

    added custom action- change - to tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do 
+    member do 
+      put :change
+    end
+  end
   devise_for :users
   root 'pages#home'
+
+
 
   get 'about' => 'pages#about'
 
